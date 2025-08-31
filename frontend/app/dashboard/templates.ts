b77a03417726af631f2ce02e3368348020ff9750
Hazimleets <hazimansari88@gmail.com>
2025-08-31 19:31:56 +0500

    Create templates.ts

---
+++ frontend/app/dashboard/templates.ts
@@ -0,0 +1,12 @@
+
+// frontend/app/dashboard/create/templates.ts
+export const templates: Record<string, { id: string; name: string }[]> = {
+  proposal: [
+    { id: 'classic', name: 'Classic' },
+    { id: 'modern', name: 'Modern' },
+  ],
+  pitchdeck: [
+    { id: 'classic', name: 'Classic' },
+    { id: 'modern', name: 'Modern' },
+  ],
+};
