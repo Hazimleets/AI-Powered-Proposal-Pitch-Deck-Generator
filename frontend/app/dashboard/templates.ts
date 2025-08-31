ed0a4e80a44b6897f9284b2fb3ee5c1a46f7eb1a
Hazimleets <hazimansari88@gmail.com>
2025-08-31 19:38:01 +0500

    Update templates.ts

--- frontend/app/dashboard/templates.ts
+++ frontend/app/dashboard/templates.ts
@@ -1,5 +1,4 @@
-
-// frontend/app/dashboard/create/templates.ts
+// frontend/app/dashboard/templates.ts
 export const templates: Record<string, { id: string; name: string }[]> = {
   proposal: [
     { id: 'classic', name: 'Classic' },
